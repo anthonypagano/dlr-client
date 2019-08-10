0ed985ab19b8625b20e587a14f950248f3bdfa08
Anthony Pagano <apagano51@gmail.com>
2019-08-10 16:53:38 -0400

    connectDb: initial commit for connecting the db

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost:27017/dlr-quotes';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/dlr-quotes';
+exports.PORT = process.env.PORT || 8080; 
 exports.API_ORIGIN = 'http://localhost:8080/';
